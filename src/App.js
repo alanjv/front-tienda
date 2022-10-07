3c49726af875164218bef4bed07fe298e7100869
alanjv3c <alanflores12@hotmail.com>
2022-10-07 12:15:34 -0600

    login y configuraciones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,174 @@
-import logo from './logo.svg';
+import { Component } from "react";
+import { usuarioServicio } from "./servicios/usuarioServicio";
+import { InputText } from 'primereact/inputtext';
+import { defaultUser } from "./config/config";
 import './App.css';
+import { Button } from 'primereact/button';
+import Swal from "sweetalert2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios: [],
+      usuario: '',
+      contrasena: '',
+      categorias: [],
+      categoria: '',
+      productos: [],
+      producto: [],
+      isAuth: false,
+      errorPassword: false,
+      errorUser: false,
+      confirmButtonText: 'Acetpar'
+    }
+    this.usuarioServicio = new usuarioServicio()
+    this.noAppiConect = true
+
+  }
+
+  componentWillUnmount = () => {
+    if (this.noAppiConect) {
+      this.setState({ usuarios: defaultUser })
+    } else {
+      this.usuarioServicio.obtenerUsuario().then(data => this.setState({ usuarios: data }))
+    }
+  }
+
+  login = () => {
+
+    return (
+      <>
+        <div className="App">
+          <div className="p-field p-grid">
+            <label htmlFor="user" className="p-col-fixed" style={{ width: '100px' }}>Usuario</label>
+            <div className="p-col">
+              <InputText id="user" onChange={(e) => this.handleUserChange(e)} type="text" />
+            </div>
+          </div>
+          <div className="p-field p-grid">
+            <label htmlFor="contrasena" className="p-col-fixed" style={{ width: '100px' }}>Contraseña</label>
+            <div className="p-col">
+              <InputText id="contrasena" onChange={(e) => this.handlePasswordChange(e)} type="password" />
+            </div>
+          </div>
+          <div className="m-top">
+            <Button label="Ingresar" onClick={() => this.validateUser()} />
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  handleUserChange = (e) => {
+    if (e.target.value != '') {
+      this.setState({ usuario: e.target.value, errorUser: false })
+    }
+  }
+
+  handlePasswordChange = (e) => {
+    if (e.target.value != '') {
+      this.setState({ contrasena: e.target.value, errorPassword: false })
+    }
+  }
+
+  validateUser = () => {
+    let validate
+    let user = this.state.usuario
+    let password = this.state.contrasena
+    if (user != '') {
+      if (password != '') {
+        validate = true
+      } else {
+        this.setState({
+          alertDialog: true,
+          type: 'alert',
+          message: 'Ingrese su contraseña',
+          errorPassword: true
+        })
+        validate = false
+      }
+
+    } else {
+      this.setState({
+        alertDialog: true,
+        type: 'alert',
+        message: 'Ingrese un usuario',
+        errorUser: true,
+
+      })
+      validate = false
+    }
+
+    if (validate) {
+      let userExist = defaultUser.length > 0
+      let findUser = userExist && defaultUser.find(usuario => usuario.usuario === user)
+
+
+
+      let validatePassword = findUser && findUser.contrasena === password
+
+      if (validatePassword) {
+        this.setState({ isAuth: true })
+
+      }
+
+      if (!findUser) {
+        this.setState({
+          alertDialog: true,
+          type: 'alert',
+          message: 'usuaio no existe, desea crearlo',
+          errorUser: true,
+          confirmButtonText: 'Crear',
+          cancelButonText: 'Cancelar'
+        })
+      }
+    }
+  }
+
+
+
+  categories = () => {
+    return (
+      <>
+      </>
+    )
+  }
+
+  products = () => {
+    return (
+      <>
+      </>
+    )
+  }
+
+  alertDialog = () => {
+    Swal.fire({
+      title: 'Error!',
+      text: this.state.message,
+      icon: this.tipe,
+      confirmButtonText: this.state.confirmButtonText,
+      cancelButonText: this.state.cancelButonText,
+
+    }).then(() => {
+      this.setState({ alertDialog: false })
+    }, (dismiss) => {
+      if (dismiss == 'cancel') {
+        // function when cancel button is clicked
+      }
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.alertDialog && this.alertDialog()}
+        {!this.state.isAuth && this.login()}
+        {this.state.isAuth && this.categories()}
+        {this.state.isAuth && this.products()}
+      </>
+    )
+  }
 }
-
-export default App;
---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+export const modeWeb = 'dev'
+export const urlDeveloper = 'https://localhost:8080/'
+export const urlProduction = ''
+export const defaultUser = [{ idtbl_usuarios: 1, usuario: 'admin', contrasena: 'admin' }]
+export const defaultCategorias = [{ idtbl_categoria: 1, categoria: 'Camisas' },
+{ idtbl_categoria: 2, categoria: 'Pantalones' },
+{ idtbl_categoria: 3, categoria: 'Calzado' }]
+
+export const defaultProducts = [{ idtbl_productos: 1, idtbl_categoria: 1, nombre: 'Camisa Tipo Polo', descripcion: '', url_imagen: '' },
+{ idtbl_productos: 2, idtbl_categoria: 2, nombre: 'Pantalon Jeam', descripcion: '', url_imagen: '' },
+{ idtbl_productos: 3, idtbl_categoria: 2, nombre: 'Pantalon De tela', descripcion: '', url_imagen: '' },
+{ idtbl_productos: 4, idtbl_categoria: 3, nombre: 'Tennis', descripcion: '', url_imagen: '' },
+{ idtbl_productos: 5, idtbl_categoria: 3, nombre: 'Zapato Formal', descripcion: '', url_imagen: '' },
+{ idtbl_productos: 6, idtbl_categoria: 1, nombre: 'Camisa Cuello V', descripcion: '', url_imagen: '' },
+{ idtbl_productos: 7, idtbl_categoria: 3, nombre: 'Zapato de dama', descripcion: '', url_imagen: '' },
+{ idtbl_productos: 8, idtbl_categoria: 2, nombre: 'Pantalon Casual', descripcion: '', url_imagen: '' },
+{ idtbl_productos: 9, idtbl_categoria: 1, nombre: 'Camisa Formak', descripcion: '', url_imagen: '' }
+]
